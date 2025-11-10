044fa5fa26de15cab315540e0b51b84ab3bdc310
허영훈 <huny4daniel@gmail.com>
2025-11-10 17:17:37 +0900

    StudyLog (2025.11.10.)

--- java-collegeclass/JavaB/1028/1028_Animal.java
+++ java-collegeclass/JavaB/1028/1028_Animal.java
@@ -1,125 +1,147 @@
-// class Animal {
-// 	String name;
-// 	int age;
-// 	private int num;
+class Animal {
+	String name;
+	int age;
+	int num;
+	Animal() {
+	}
+	Animal(String a, int b, int c) {
+		name = a;
+		age = b;
+		num = c;
+	}
+	void setName(String a) {
+		name = a;
+	}
+	void setAge(int a) {
+		age = a;
+	}
+	void setNum(int a) {
+		num = a;
+	}
+	String getName() {
+		return name;
+	}
+	int getAge() {
+		return age;
+	}
+	int getNum() {
+		return num;
+	}
+}
 
-// 	Animal() {
-// 	}
+class Animal {
+	String name;
+	int age;
+	private int num;
+	Animal() {
+	}
+	Animal(String a, int b, int c) {
+		name = a;
+		age = b;
+		num = c;
+	}
+	void setName(String a) {
+		name = a;
+	}
+	void setAge(int a) {
+		age = a;
+	}
+	void setNum(int a) {
+		num = a;
+	}
+	String getName() {
+		return name;
+	}
+	int getAge() {
+		return age;
+	}
+	int getNum() {
+		return num;
+	}
+	void printAnimal(){
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(num);
+	}
+}
 
-// 	Animal(String a, int b, int c) {
-// 		name = a;
-// 		age = b;
-// 		num = c;
-// 	}
+class Pet extends Animal{
+	String place;
+	Pet(){
+	}
+	Pet(String a, int b, int c, String d){
+		super(a,b,c);
+		//name=a;
+		//age=b;
+		//setNum(c);
+		place=d;
+	}
+	void setPlace(String a){
+		place=a;
+	}
+	String getPlace(){
+		return place;
+	}
+}
 
-// 	void setName(String a) {
-// 		name = a;
-// 	}
+class Pet extends Animal{
+	String place;
+	Pet(){
+	}
+	Pet(String a, int b, int c, String d){
+		super(a,b,c);
+		//name=a;
+		//age=b;
+		//setNum(c);
+		place=d;
+	}
+	void setPlace(String a){
+		place=a;
+	}
+	String getPlace(){
+		return place;
+	}
+	void printPet(){
+		printAnimal();
+		//System.out.println(getName());
+		//System.out.println(getAge());
+		//System.out.println(getNum());
+		System.out.println(place);
+	}
+}
 
-// 	void setAge(int a) {
-// 		age = a;
-// 	}
+class Cat extends Pet{
+	int weight;
+	Cat(){
+	}
+	Cat(String a, int b, int c, String d, int e){
+		super(a,b,c,d);
+		weight=e;
+	}
+	void setWeight(int a){
+		weight=a;
+	}
+	int getWeight(){
+		return weight;
+	}
+}
 
-// 	void setNum(int a) {
-// 		num = a;
-// 	}
-
-// 	String getName() {
-// 		return name;
-// 	}
-
-// 	int getAge() {
-// 		return age;
-// 	}
-
-// 	int getNum() {
-// 		return num;
-// 	}
-
-// 	void printAnimal() {
-// 		System.out.println(name);
-// 		System.out.println(age);
-// 		System.out.println(num);
-// 	}
-// }
-
-// class Pet extends Animal{
-// 	String place;
-// 	Pet(){
-// 	}
-// 	Pet(String a, int b, int c, String d){
-// 		super(a,b,c);
-// 		//name=a;
-// 		//age=b;
-// 		//setNum(c);
-// 		place=d;
-// 	}
-// 	void setPlace(String a){
-// 		place=a;
-// 	}
-// 	String getPlace(){
-// 		return place;
-// 	}
-// }
-
-// class Pet extends Animal{
-// 	String place;
-// 	Pet(){
-// 	}
-// 	Pet(String a, int b, int c, String d){
-// 		super(a,b,c);
-// 		//name=a;
-// 		//age=b;
-// 		//setNum(c);
-// 		place=d;
-// 	}
-// 	void setPlace(String a){
-// 		place=a;
-// 	}
-// 	String getPlace(){
-// 		return place;
-// 	}
-// 	void printPet(){
-// 		printAnimal();
-// 		//System.out.println(getName());
-// 		//System.out.println(getAge());
-// 		//System.out.println(getNum());
-// 		System.out.println(place);
-// 	}
-// }
-
-// class Cat extends Pet{
-// 	int weight;
-// 	Cat(){
-// 	}
-// 	Cat(String a, int b, int c, String d, int e){
-// 		super(a,b,c,d);
-// 		weight=e;
-// 	}
-// 	void setWeight(int a){
-// 		weight=a;
-// 	}
-// 	int getWeight(){
-// 		return weight;
-// 	}
-// }
-
-// class Cat extends Pet{
-// 	int weight;
-// 	Cat(){
-// 	}
-// 	Cat(String a, int b, int c, String d, int e){
-// 		super(a,b,c,d);
-// 		weight=e;
-// 	}
-// 	void setWeight(int a){
-// 		weight=a;
-// 	}
-// 	int getWeight(){
-// 		return weight;
-// 	}
-// 	void printCat(){
-//         	printPet();
-// 	        System.out.println(getWeight());
-// 	}
-// }
+class Cat extends Pet{
+	int weight;
+	Cat(){
+	}
+	Cat(String a, int b, int c, String d, int e){
+		super(a,b,c,d);
+		weight=e;
+	}
+	void setWeight(int a){
+		weight=a;
+	}
+	int getWeight(){
+		return weight;
+	}
+	void printCat(){
+        printPet();
+        System.out.println(getWeight());
+	}
+}
---
+++ java-collegeclass/JavaB/SiheungStamp/SiheungStamp.java
@@ -0,0 +1,31 @@
+public class SiheungStamp {
+    
+}
+
+class User {
+    int userId;
+    int userPw;
+
+    User(){
+    }
+}
+
+class Spot {
+    String spotName;
+    int spotId;
+    int[] description;
+
+    Spot() {
+    }
+}
+
+class StampManager {
+    StampManager() {
+    }
+}
+
+class RewordSystem {
+    RewordSystem() {
+    }
+}
+
